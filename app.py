b0db8088f6da01466df1154b2ef0dc2a451b7270
Omar666-stack <129784893+Omar666-stack@users.noreply.github.com>
2024-07-20 15:35:37 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask
-app P Flask (_name_)
+app = Flask (__name__)
 
 @app.router('/')
 def hello_geek():
   return '<h1>Hello from Flask & Docker</h2>'
 
 
-  if _name_ == "_main_":
+  if __name__ == "__main__":
     app.run(debug=True)
