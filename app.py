b70b830645a5a0d2a3293a5d5311f428a33cad95
Omar Val <i2220746@continental.edu.pe>
2024-07-21 05:32:44 +0000

    despliegue

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+app P Flask (_name_)
+
+@app.router('/')
+def hello_geek():
+  return '<h1>Hello from Flask & Docker</h2>'
+
+
+  if _name_ == "_main_":
+    app.run(debug=True)
